b7f58ed3a9a656bfe8e4ba17738f655dd96ee293
Dolganov_830501 <dolganovvlad486@gmail.com>
2023-02-12 22:29:17 +0300

    react_start

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const elem = (
+  <div>
+    <button>Click</button>
+    <h2>Hello World!</h2>
+  </div>
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ elem,
+ document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
