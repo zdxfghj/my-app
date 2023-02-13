8153685985be7e341445a3da54cce0d61f50bfa9
Dolganov_830501 <dolganovvlad486@gmail.com>
2023-02-13 23:02:33 +0300

    q

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+
+import { StrictMode } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const Header = () =>{
+  return <h2>Hello World!</h2>
+}
+
+const Field = () =>{
+  const styleField = {
+    width: '300px'
+  };
+
+  return <input placeholder='Type here' type="text" style={styleField} />
+}
+
+function Btn(){
+  const logo = "log in";
+  let logger = true;
+  function res(){
+    return 'second level';
+  }
+
+  return <button>Click {logo} {res()} {logger? "enter": logo} </button>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StrictMode>
+        <Header/>
+      </StrictMode>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
 export default App;
+
